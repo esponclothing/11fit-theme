7dede263ed123b41f5f489c7deeee63a8066f464
espon <clothingespon@gmail.com>
2026-07-22 18:09:16 +0530

    Fix single product add to cart and pass customer details to Shopify checkout

--- assets/product-form.js
+++ assets/product-form.js
@@ -44,7 +44,7 @@ if (!customElements.get('product-form')) {
         }
         config.body = formData;
 
-        fetch(`${routes.cart_add_url}`, config)
+        fetch(window.Shopify.routes.root + 'cart/add.js', config)
           .then((response) => response.json())
           .then((response) => {
             if (response.status) {
