972cee42a986461742d8f720ba55bd34c1c098b3
espon <clothingespon@gmail.com>
2026-07-25 12:10:37 +0530

    Fix multiple Add to Cart clicks for Meta Pixel tracking

--- assets/product-form.js
+++ assets/product-form.js
@@ -27,6 +27,7 @@ if (!customElements.get('product-form')) {
 
         this.submitButton.setAttribute('aria-disabled', true);
         this.submitButton.classList.add('loading');
+        this.submitButton.style.pointerEvents = 'none';
         this.querySelector('.loading__spinner').classList.remove('hidden');
 
         const config = fetchConfig('javascript');
@@ -117,6 +118,7 @@ if (!customElements.get('product-form')) {
           })
           .finally(() => {
             this.submitButton.classList.remove('loading');
+            this.submitButton.style.pointerEvents = 'auto';
             if (this.cart && this.cart.classList.contains('is-empty')) this.cart.classList.remove('is-empty');
             if (!this.error) this.submitButton.removeAttribute('aria-disabled');
             this.querySelector('.loading__spinner').classList.add('hidden');
